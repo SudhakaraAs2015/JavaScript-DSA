8149e7437ee08b4be12aaa5c3b61f9cbc3f027ab
Sudhakara H N <sudhakarahn29@gmail.com>
2025-01-09 22:56:15 +0530

    Bubble Sorting

---
+++ SortingAlgorithms/BubbleSorting/bubbleSorting.js
@@ -0,0 +1,20 @@
+function bubbleSorting(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
+
+const arr = [8, 20, -2, 4, -6];
+bubbleSorting(arr);
+console.log(arr);
+
+//Big-O = O(n^2)  Number of elements increases the number of comparision increses by sqaure of the number
