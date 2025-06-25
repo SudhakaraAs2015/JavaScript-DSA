eed2f55fcd42400864efe4ce3e2b4788c4592b68
Sudhakara H N <sudhakarahn29@gmail.com>
2025-06-25 09:25:09 +0530

    Used Other iterative approach for Fibnacci

--- fibannacii/fibnacci.js
+++ fibannacii/fibnacci.js
@@ -1,11 +1,9 @@
-function fibanacci(n){
-   let fib = [0,1];
-   for(let i =2 ; i<n ; i++){
-    fib[i] = fib[i-1]+ fib[i-2];
-   }
-   return fib;
-    
-    
+function fibanacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
 }
 
 console.log(fibanacci(2));
@@ -13,16 +11,30 @@ console.log(fibanacci(3));
 console.log(fibanacci(11));
 console.log(fibanacci(4));
 
-
 // Big-O = O(n)
-
-// function fibonacci(n) {
-//     if (n <= 1) {
-//       return n;
-//     }
-//     return fibonacci(n - 1) + fibonacci(n - 2);
+// Other Approacch
+// function fib(n) {
+//   if (n <= 0) {
+//     return 0;
 //   }
-  
-//   console.log(fibonacci(3));  // Output: 55
-  
+//   if (n == 1) {
+//     return 1;
+//   }
+//   let a = 0,
+//     b = 1,
+//     temp;
 
+//     for(let i =2; i<=n; i++){
+//       temp = a+b
+//       a = b;
+//       b = temp;
+//     }
+//     return b;
+// };
+
+// console.log(fib(2));
+// console.log(fib(3));
+// console.log(fib(11));
+// console.log(fib(4));
+
+//Big = O(n)
