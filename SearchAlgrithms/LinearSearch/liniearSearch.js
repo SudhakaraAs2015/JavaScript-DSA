1dbdcd4bae0d126d45aeeae0ce4e34e425a957c6
Sudhakara H N <sudhakarahn29@gmail.com>
2025-07-23 07:27:50 +0530

    Small Changes

--- SearchAlgrithms/LinearSearch/liniearSearch.js
+++ SearchAlgrithms/LinearSearch/liniearSearch.js
@@ -1,14 +1,16 @@
-function linearSearch(arr,target){
-    for(let i =0; i<arr.length; i++){
-        if(arr[i] === target){
-            console.log(arr[i]);
-            return i
-        }
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Checking index ${i}: value = ${arr[i]}`);
+    if (arr[i] === target) {
+      console.log(`Found ${target} at index ${i}`);
+      return i;
     }
-    return -1;
-};
+  }
+  console.log(`${target} not found in the array.`);
+  return -1;
+}
 
-console.log(linearSearch([-2,3,5,1,3,5],1));
-console.log(linearSearch([-2,3,5,1,3,5],5));
+console.log(linearSearch([-2, 3, 5, 1, 3, 5], 1));
+console.log(linearSearch([-2, 3, 5, 1, 3, 5], 5));
 
-//Big-O = O(n)  Linear
+// Big-O = O(n)  Linear
