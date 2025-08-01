559d44d50c8619672de364b74256749706454f4c
Sudhakara H N <sudhakarahn29@gmail.com>
2025-08-01 07:32:41 +0530

    Sorting Algorithm

---
+++ SortingAlgorithms/QuickSorting/quickSorting.js
@@ -0,0 +1,21 @@
+function quickSorting(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length-1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSorting(left),pivot, ...quickSorting(right)];
+}
+console.log(quickSorting([8, 20, -2, 4, -6]));
+
+//Worst case - O(n^2)
+// Avg Case -O(logn)
